897343fc293d1a93b8a0b17cb0e5b777225d036e
waledzee <waledzaki201@gmail.com>
2025-05-25 21:31:40 +0300

    structure of project

---
+++ src/main/java/com/example/Ivestment_Stock_Api/IvestmentStockApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.Ivestment_Stock_Api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IvestmentStockApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IvestmentStockApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Ivestment_Stock_Api/IvestmentStockApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Ivestment_Stock_Api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IvestmentStockApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
